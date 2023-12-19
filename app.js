3d4f08c07e7fa350496f90f1cd227a5b8e49efea
Xotam005 <151056064+Xotam005@users.noreply.github.com>
2023-12-19 23:32:07 +0500

    dsads

---
+++ app.js
@@ -0,0 +1,28 @@
+var imya = prompt(' Bведите своё имя ');
+
+console.log(' Меня зовут ' + imya);
+
+var age = +prompt('Введите свой возраст')
+
+console.log(' Мне ' + age);
+
+var exercise = +prompt('Решите пример ' + '20 + 3 = ?');
+console.log('Пример 1.' +  ' 20 + 3 = ' + (20 + 3)  + ' Правильный ответ: ' + exercise);
+
+var exercise = +prompt('Решите пример ' + '20 - 3 = ?');
+console.log('Пример 2.' +  ' 20 - 3 = ' + (20 - 3)  + ' Правильный ответ: ' + exercise);
+
+var exercise = +prompt('Решите пример ' + '5 * 3 = ?');
+console.log('Пример 3.' +  ' 20 * 3 = ' + (5 * 3)  + ' Правильный ответ: ' + exercise);
+
+var exercise = +prompt('Решите пример ' + '15 / 3 = ?');
+console.log('Пример 4.' +  ' 20 / 3 = ' + (15 / 3)  + ' Правильный ответ: ' + exercise);
+
+var exercise = +prompt('Решите пример ' + '10 % 5 = ?');
+console.log('Пример 5.' +  ' 20 + 3 = ' + (10 % 5)  + ' Правильный ответ: ' + exercise);
+
+alert(imya, age, exercise);
+
+
+
+
